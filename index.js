3ba9dcd390fb1f827ec2bab2409e498d80eea19e
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-09 16:27:38 -0300

    Criação do endpoint POST para criação de mensagens

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
- 
+
 app.get('/mensagens', function (req, res) {
   res.send('Exibir a lista de mensagens.')
 })
- 
+
+app.post('/mensagens', function (req, res) {
+  res.send('Criar uma mensagem.')
+})
+
 app.listen(3000)
